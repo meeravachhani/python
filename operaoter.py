96ac382083dca502dde77a1fe844d7b49d8de6a1
meeravachhani <meeravachhani02@gmail.com>
2025-01-30 11:11:02 +0530

    operaoter.py

---
+++ operaoter.py
@@ -0,0 +1,64 @@
+a=10
+b=5
+c=20
+ # arithmetic
+print("addition to value",a+b)
+print("subtrastion ",a-b)
+print("multiplication",a*b)
+print("divistion",a/b)
+print("modual",a%b)
+print("power",a**b)
+print("sumthing",a//b)
+
+# assiment
+
+a=2
+print(a)
+
+a+=5
+print(a)
+
+a-=1
+print(a)
+
+a*=2
+print(a)
+
+a/=2
+print(a)
+
+a*=2
+print(a)
+
+a%=2
+print(a)
+
+a//=2
+print(a)
+
+a**=2
+print(a)
+
+# compsrision
+a=10
+b=5
+print("equeal", a==b)
+print("not equeal",a!=b)
+print("lesthen",a<b)
+print("graterthen",a>b)
+print("leassorqueal",a<=b)
+print("grateror qqeueal",a>=b)
+
+# logical
+print(a>b and a>c)
+print(a<b or a<c)
+print(not(a>b and a>c) )
+
+# identity
+print(a is b)
+print(a is not b)
+
+
+
+
+
