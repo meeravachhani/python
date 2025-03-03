d8c977fa58cfeef92ab52f9935533aa43af708fc
meeravachhani <meeravachhani02@gmail.com>
2025-03-03 09:06:18 +0530

    Create set2.py

---
+++ set2.py
@@ -0,0 +1,30 @@
+# 1)
+
+# a='hello good morning,python is  a \t open sorce ,is a very easy.'
+# print(a)
+# b='hello good morning,python is  a \n open sorce ,is a very easy.'
+# print(b)
+
+# # 2)
+# a=input('enter the string:-')
+# print(a)
+# print(a[2])
+# print(a[4])
+
+# 3)
+# a='2,4,5'
+# print(a)
+
+# 4)
+# a=2
+# b=16
+# c=a*7/b+a
+# print(c)
+
+# 5)
+# import sys
+# a=sys.argv
+# print(a)
+
+# 6)
+import sys
