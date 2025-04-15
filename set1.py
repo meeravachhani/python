3518a5e809aa89b508bdd68ac088b8778dfd9ede
meeravachhani <meeravachhani02@gmail.com>
2025-04-15 11:17:51 +0530

    Create set1.py

---
+++ set1.py
@@ -0,0 +1,52 @@
+# # 1) write a program that print your name and your college name.
+# print('my name is meera')
+# a=input('enter your name:- ')
+# print(a)
+
+# # 2)write a program to prints your address with name,all in new lines
+# print('my name is meera \n address:- \n to-juthal, \n dis:-junagadh')
+# a=input('enter your name:- \n')
+# b=input('enter your address:- \n')
+# print(a,'\n',b)
+
+# # 3) write a program that accept two numbers and perform all basic mathematical operations.
+# a=int(input('enter first number:- '))
+# b=int(input('enter second number:- '))
+# print('addition of two value:-',a + b)
+# print('subtraction of two value:- ',b-a)
+# print('multiplication of two value:- ',a*b)
+# print('diviction of two value:- ',a/b)
+# print('modular of two value:- ',b/a)
+
+
+# # 4)write a  program to calculate simple intrest
+# principle=int(input('enter the principle amount'))
+# year=int(input('enter the year of amount'))
+# rate=int(input('enter the rate of intrest'))
+# intrest=(principle*year*rate)/100
+# print(intrest)
+
+# # # 5)write a program to calculate 10% bouns of salary.
+# salary = float(input("Enter the salary: "))
+# bonus = salary * 0.10
+# print(f"The 10% bonus on a salary of ",salary ," is:", bonus)
+
+
+# # # 6)write a program to convert km to meter.
+# km=float(input('enter km:-'))
+# meter=km*1000
+# print(meter,"meter ")
+
+# 7)the distance between two cities is input through keyboard. 
+# write a convert to program and print this distance in feet,meter ,invh and centimeter.
+
+
+
+
+
+
+
+
+
+
+
