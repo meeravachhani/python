23fc1545c0bb8986d5bb9731dff056914b927ad0
meeravachhani <meeravachhani02@gmail.com>
2025-04-15 11:18:58 +0530

    Update set2.py

--- set2.py
+++ set2.py
@@ -28,3 +28,90 @@
 
 # 6)
 import sys
+# # 8)
+
+
+# b=0o234
+# a=int(b)
+# print(a)
+
+# c=0b010100111
+# b=int(c)
+# print(b)
+
+# x=22.2
+# a=int(x)
+# print(a)
+
+# # 9)
+# a=['meera',2,5,8,'abcd',9,1]
+# print(a)
+# print(type(a))
+# print(a[0])
+# print(a[-4])
+# a[4]=10
+# print(a)
+
+# # 10)
+# a=(8,5,7,3,1,4,2)
+# print(a)
+# # a[1]=2
+# print(a[2:5])
+# print(a[-5:-2])
+
+# # 11)
+# a={1:'meera',2:'disha',3:'tirth',4:'ansh',5:'kruti',6:'vrunda',7:'dax',8:'keval',9:'madhur',10:'xyz'}
+# print(a)
+# print(type(a))
+# print(a[7])
+# print(a.values())
+# a[7]='abc'
+# print(a)
+
+
+
+
+# 12)
+# a={1,4,6,5,8,9,9}
+# print(type(a))
+# print(a)
+# a={1,4,6,5,8,9,9,12,21}
+# print(a)
+# a.remove(4)
+# print(a)
+
+# # 14)
+# a=[2,4,3,6,7]
+# print(a)
+# b=bytes(a)
+# print(type(b))
+# print(a[-5])
+# print(a[-1])
+# # a[5]=12
+# # print(a)
+# a[4]=22
+# print(a)
+
+
+# # 15)
+# a=[8,9,5,0,3,1,7]
+# print(a)
+# b=bytearray(a)
+# print(type(b))
+# print(a[-7])
+# print(a[6])
+
+
+# b[6]=11
+# print(b)
+# print()
+# a.remove(7)
+# print(a)
+
+
+
+
+
+
+
+
