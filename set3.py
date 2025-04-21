bf346d035684107ab603921c4c5d3aa13159d014
meeravachhani <meeravachhani02@gmail.com>
2025-04-21 07:43:14 +0530

    Create set3.py

---
+++ set3.py
@@ -0,0 +1,101 @@
+# # 2)
+# import sys  
+# print(sys.argv)
+# a=int(sys.argv[1])
+# b=int(sys.argv[2])
+# c=int(sys.argv[3])
+# d=int(sys.argv[4])
+# e=int(sys.argv[5])
+
+# per=0
+# sum=a+b+c+d+e
+# print(sum)
+# per=(sum/500)*100
+# print(per)
+
+# # 3
+# a=int(input('enter water in liters:'))                 
+# if(a<=90):
+# 	print('no pay')
+# elif(a>90 and a<=150):
+# 	print('per ltr 2')
+# 	sum=a*2
+# 	print(sum)
+# elif(a>150 and a<=250):
+# 	print('per ltr 5')
+# 	sum=a*5
+# 	print(sum)
+# elif(a>250):
+# 	print('per ltr 10')
+# 	sum=a*10
+# 	print(sum)
+# else:
+# 	print('invalid')
+
+# # 4
+# a=int(input('enter student mark:- '))
+# if(a>=90):
+# 	print('a1 grade')
+# elif(a>=80 and a<90):
+# 	print('a grade')
+# elif(a>=70 and a<80):
+# 	print('b1 grade')
+# elif(a>60 and a<70):
+# 	print('b grade')
+# elif(a>=50 and a<60):
+# 	print('can do bater!')
+# else:
+# 	print('need to hard work')
+
+# # 5)
+# income=int(input('enter your income'))
+# if income<=800000:
+# 	print('no txt')
+# elif(income>800000 and income<=1000000):
+# 	print('15% txt')
+# 	print(income*0.15)	
+# elif(income>1000000 and income<=2000000):
+# 	print('20% txt')
+# 	print(income*0.2)
+# elif(income>2000000):
+# 	print('30% txt')
+# 	print(income*0.3)
+# else:
+# 	print('incoreect')
+
+
+# 6)
+# import sys  
+# print(sys.argv)
+# num1=int(sys.argv[1])
+# num2=int(sys.argv[2])
+# print(min(sys.argv))
+# print(num1)
+# print(num2)
+
+# 7)
+# mark1=int(input('enter mark1:'))
+# mark2=int(input('enter mark2:'))
+# mark3=int(input('enter mark3:'))
+# mark4=int(input('enter mark4:'))
+# print(mark1)
+# print(mark2)
+# print(mark3)
+# print(mark4)
+
+
+lst=input('enter number').spilt(' ')
+num=list(lst)
+print(num)
+
+
+
+
+
+
+
+
+
+
+
+
